03ef862ecb2db699dc128c676e9d1a8c60b9e37b
yueweilu <kulang3@163.com>
2017-10-31 22:19:11 +0800

    创建子弹

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -6,3 +6,9 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230,230,230)
+        self.ship_speed_factor = 10
+        #子弹的设置
+        self.bullet_speed_factor = 5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
